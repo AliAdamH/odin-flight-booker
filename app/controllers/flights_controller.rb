ec2efc7d4a85d2c3254f5aac1ebd46aebf1cf7aa
Ali Hassaine <ali.hs@outlook.fr>
2022-04-03 13:01:50 +0000

    Add flight search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,6 +3,17 @@ class FlightsController < ApplicationController
 
   def index
     @airports = Airport.all.order(:code)
+    @flights_dates = Flight.all.map(&:start)
+    @days = @flights_dates.reduce(Set.new) {|acc,date| acc.add date.day}.sort
+    @months = @flights_dates.reduce(Set.new) {|acc,date| acc.add date.month }.sort
+    @years = @flights_dates.reduce(Set.new) {|acc,date| acc.add date.year }.sort
+    @available_flights = Flight.available_flights(
+      departure: params[:departure_code],
+      arrival: params[:arrival_code],
+      day: params[:day],
+      month: params[:month],
+      year: params[:year],
+    ) if params[:commit]
   end
 
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,14 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+
+
+  def self.available_flights(day:, month:, year:, departure:, arrival:)
+    formatted_time = Time.new(year, month, day)
+    where(
+      departure_airport_id: departure,
+      arrival_airport_id: arrival,
+      start: formatted_time..(formatted_time + 1.day).midnight
+    )
+  end
 end
