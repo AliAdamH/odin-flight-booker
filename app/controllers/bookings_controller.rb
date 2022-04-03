d7108c3a854666c8453c76790e3d4a86fbce3ce0
Ali Hassaine <ali.hs@outlook.fr>
2022-04-03 18:12:05 +0000

    Add booking creation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,6 +11,18 @@ class BookingsController < ApplicationController
 
 
   def create
-    redirect_to root_path
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking, notice: 'You have succesfully booked your flight'
+    else
+      redirect_to root_path
+    end 
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: %i[name email])
   end
 end
