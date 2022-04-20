202e0788183d1eb4bd3cac6f51ca2d99ba0dd0eb
Ali Hassaine <ali.hs@outlook.fr>
2022-04-20 10:56:13 +0000

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,26 +8,31 @@
 
 
 # Step 1: Create  Airports.
-require 'set'
-require 'json'
-file = File.read '../airports.json'
-data_hash = JSON.parse file
 
-airports_set = Set.new
-110.times do
-  airports_set.add data_hash.sample['iata']
+airports_codes = %w[ATL DFW DEN ORD LAX CLT MCO CAN CTU LAS PHX MIA IST SZX SEA MEX CKG IAH SHA PEK]
+
+airports = []
+airports_codes.each do |airport|
+  airports << Airport.create(code: airport)
 end
 
-airports_set.each do |airport|
-  Airport.create(code: airport)
+
+# Step 2: Create Flights (20*20 Flights)
+airports.each do |departure|
+  airports.each do |destination|
+    next if departure == destination
+
+    Flight.create(
+      departure_airport: departure,
+      arrival_airport: destination,
+      start: Faker::Time.forward(days: 30),
+      duration: rand(600)
+    )
+
+    end
 end
 
-# Step 2: Create Flights (50 Flights)
-(2..100).step(2) do |i|
-  Flight.create(
-    departure_airport_id: i,
-    arrival_airport_id: i - 1,
-    start: Faker::Time.forward(days: 30),
-    duration: rand(600)
-  )
-end
+
+
+
+
