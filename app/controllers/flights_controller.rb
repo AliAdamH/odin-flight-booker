0e4bcf70b32ba534eaad647ae43da5db09b5e805
Ali Hassaine <ali.hs@outlook.fr>
2022-04-20 09:28:51 +0000

    Remove redundant sort

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,10 +3,10 @@ class FlightsController < ApplicationController
 
   def index
     @airports = Airport.all.order(:code)
-    @flights_dates = Flight.all.map(&:start)
-    @days = @flights_dates.reduce(Set.new) {|acc,date| acc.add date.day}.sort
-    @months = @flights_dates.reduce(Set.new) {|acc,date| acc.add date.month }.sort
-    @years = @flights_dates.reduce(Set.new) {|acc,date| acc.add date.year }.sort
+    @flights_dates = Flight.all.order(:start).map(&:start)
+    @days = @flights_dates.reduce(Set.new) { |acc, date| acc.add date.day }.sort
+    @months = @flights_dates.reduce(Set.new) { |acc, date| acc.add date.month }
+    @years = @flights_dates.reduce(Set.new) { |acc, date| acc.add date.year }
     @available_flights = Flight.available_flights(
       departure: params[:departure_code],
       arrival: params[:arrival_code],
